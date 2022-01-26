80519c121815afa3302633be4381c146615123a6
Robin Scherrer <mail@robinscherrer.com>
2022-01-26 13:44:44 +0100

    CO-15 lint code and fix errors

--- controllers/dnsrecord_controller.go
+++ controllers/dnsrecord_controller.go
@@ -136,7 +136,7 @@ func (r *DNSRecordReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	}
 
 	// Check if DNS record proxied is true and ttl is not 1
-	if *instance.Spec.Proxied == true && instance.Spec.TTL != 1 {
+	if *instance.Spec.Proxied && instance.Spec.TTL != 1 {
 		log.Info("DNSRecord is proxied and ttl is not 1")
 		instance.Status.Phase = "Failed"
 		instance.Status.Message = "DNSRecord is proxied and ttl is not 1"
